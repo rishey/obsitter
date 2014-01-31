942efd3b5262cff1fd219ac5e342185f72322af8
Richard Bluestein <richard@bluestein.com>
2014-01-31 00:22:01 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Obsitter::Application.config.secret_key_base = '17307bf103b3fa28e3d980e6f04ea39670c93585e052adaaf80ea3f21c3b9cb0881b4aeacf2c3b405a21077d29cd20a0a3915a34deab14ad7d6569231a76a8aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Obsitter::Application.config.session_store :cookie_store, key: '_obsitter_session'
