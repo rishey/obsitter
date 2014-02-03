5d8308e95100f9b550a2af2908eb81fbf84b5598
Richard Bluestein <richard@bluestein.com>
2014-02-03 13:05:38 -0600

    login and logout working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,22 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    p "RUDOLF"
+    p params
+    p user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      p session[:user_id] = user.id
+      p "session id"
+      p session[:user_id]
+      redirect_to :root
+    else
+      @error = "Incorrect username or password"
+      p @error
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to :root
