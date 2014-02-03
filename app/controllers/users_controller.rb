2a720c9feae6e7d599378a209933038a0693dc24
Richard Bluestein <richard@bluestein.com>
2014-02-03 11:44:09 -0600

    can create user via session controller and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      @signup_errors = @user.errors.full_messages
+      render "sessions/new"
+    end
+  end
+
+ private
+
+   def user_params
+       params.require(:signup).permit(:username, :email, :password, :password_confirmation, :protect_tweets, :full_name, :web_site, :bio)
+   end
 
 end
