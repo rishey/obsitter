27ae757b55d77f356e5c56589c795933c40faf28
Richard Bluestein <richard@bluestein.com>
2014-02-03 17:12:12 -0600

    added css for navbar and fixed error i made in sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,16 @@
 class SessionsController < ApplicationController
   def new
-    p session[:user_id]
-    p current_user
+    # p session[:user_id]
+    # p current_user
   end
 
   def create
-    p "RUDOLF"
-    p params
-    p user = User.find_by(username: params[:username])
+    user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      p session[:user_id] = user.id
-      p "session id"
-      p session[:user_id]
+      session[:user_id] = user.id
       redirect_to :root
     else
       @error = "Incorrect username or password"
-      p @error
       render :new
     end
   end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,12 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  # def logged_in?
+  #   if current_user
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
 end
