92059f27b42d7dbcbfce16ef2335ea7b2b1e5e83
Richard Bluestein <richard@bluestein.com>
2014-02-04 00:18:27 -0600

    schema mod

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140131071850) do
+ActiveRecord::Schema.define(version: 20140204054241) do
+
+  create_table "special_words", force: true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
